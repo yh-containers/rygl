3abe45d497a7835c37bff9fa6b04f97b8bfee831
slh <1104753531@qq.com>
2018-12-18 14:18:39 +0800

    edit database

---
+++ application/common/model/Admin.php
@@ -0,0 +1,7 @@
+<?php
+namespace app\common\model;
+
+class Admin extends Base
+{
+    protected $name = 'sys_admin';
+}
--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -12,4 +12,9 @@ class Index
     {
         return 'hello,' . $name;
     }
+
+    public function test()
+    {
+        dump(model('Admin')->select());
+    }
 }

04153788203cd3887a41e39ab529ac205d6b25f8
slh <1104753531@qq.com>
2018-12-25 17:49:15 +0800

    add
    添加公司api

---
+++ application/api/controller/Company.php
@@ -0,0 +1,19 @@
+<?php
+namespace app\api\controller;
+
+class Company extends Common
+{
+    //获取公司作息时间
+    public function info()
+    {
+        $fields = $this->request->param('field','*');
+        $visible = ['id','uid','name','logo','area_id','address','industry_id',
+            'description','website','contact','phone','coordinate','sign_mac','work_time','create_time'];
+
+        $model = new \app\common\model\Company();
+        $info = $model->field($fields)->visible($visible)->find($this->company_id);
+
+        return jsonOut('获取成功',1,$info);
+    }
+}
+
